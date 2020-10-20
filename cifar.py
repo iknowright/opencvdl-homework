fda01550a569442da96a3ab7c1b29e93d8d44464
iknowright <changchaishi@gmail.com>
2020-10-21 00:43:27 +0800

    add cifar sample

--- cifar.py
+++ cifar.py
@@ -1,6 +1,7 @@
 import pickle
 import random
 
+from tensorflow.keras.datasets.cifar10 import load_data
 from tensorflow.keras.optimizers import SGD
 from tensorflow.keras import utils
 from tensorflow.keras.models import Sequential, load_model
@@ -12,46 +13,39 @@ from tensorflow.keras.layers import MaxPooling2D
 import numpy as np
 import matplotlib.pyplot as plt
 
-# cifar-10-python.tar.gz
-def unpickle(file):
-    with open(file, 'rb') as fo:
-        dict = pickle.load(fo, encoding='bytes')
-    return dict
-
-
-files = [f"data_batch_{i+1}" for i in range(5)]
-files.append("test_batch")
-
 class Quiz5():
     def __init__(self):
-        self.batch_size = 32
-        self.learning_rate = 0.001
+        self.batch_size = 256
+        self.learning_rate = 0.1
         self.optimizer = 'SGD'
         self.model = None
 
+    def unpickle(self, file):
+        with open(file, 'rb') as fo:
+            data = pickle.load(fo, encoding='bytes')
+        return data
+
+    def get_model_data(self):
+        with open('src/Q5_Data/cifar10.data', 'rb') as fo:
+            data = pickle.load(fo)
+            (x_train, y_train), (x_test, y_test) = data
+            return x_train, y_train, x_test, y_test
+
     def plot_random(self):
-        meta_data = unpickle(f"src/Q5_Data/batches.meta")
+        meta_data = self.unpickle(f"src/Q5_Data/batches.meta")
         label_names = meta_data[b'label_names']
         print(label_names)
 
-        batch1 = unpickle(f"src/Q5_Data/{files[0]}")
-        # dict_keys([b'batch_label', b'labels', b'data', b'filenames'])
-        print(batch1.keys())
-        print(type(batch1[b'labels']))
+        x_train, y_train, _, _ = self.get_model_data()
+
+        print(y_train.shape)
+        indices = random.sample(range(50000), k=10)
         plt.figure(1)
-        for i in range(10):
+        for i, index in enumerate(indices):
             plt.subplot(1, 10, i+1)
-            print(i)
-            index = random.randint(0, len(batch1[b'labels']))
-            print(index)
-            label = batch1[b'labels'][index]
+            label = y_train[index][0]
             label_name = label_names[label].decode('ASCII')
-            print(batch1[b'data'].shape)
-            image_buff = batch1[b'data'][index].reshape(3, 32, 32)
-            image_buff = np.swapaxes(image_buff, 0, 2)
-            image_buff = np.swapaxes(image_buff, 0, 1)
-            print(image_buff.shape)
-            plt.imshow(image_buff)
+            plt.imshow(x_train[index])
             plt.title(label_name)
 
         plt.show()
@@ -88,67 +82,41 @@ class Quiz5():
         model.summary()
         self.model = model
 
-
     def parameter(self):
-        self.batch_size = 32
-        self.learning_rate = 0.001
-        self.optimizer = 'SGD'
         print("Hyperparameters:")
-        print(f"batch_size: {batch_size}")
-        print(f"learning_rate: {learning_rate}")
-        print(f"optimizer: {optimizer}")
-
-    def get_model_data(self):
-        # generate testing data
-        data = unpickle(f"src/Q5_Data/test_batch")
-        image_data = data[b'data'][:2000]
-        print(image_data.shape)
-        image = image_data.reshape(2000, 3, 32, 32)
-        image = np.swapaxes(image, 1, 3)
-        image = np.swapaxes(image, 1, 2)
-        y_test = data[b'labels'][:2000]
-        x_test = image
-        y_test = utils.to_categorical(y_test, 10, dtype=int)
-
-        # data_batch_1
-        # generate training data
-        data = unpickle(f"src/Q5_Data/data_batch_1")
-        image_data = data[b'data']
-        print(image_data.shape)
-        image = image_data.reshape(10000, 3, 32, 32)
-        image = np.swapaxes(image, 1, 3)
-        image = np.swapaxes(image, 1, 2)
-        y_train = data[b'labels']
-        x_train = image
-        y_train = utils.to_categorical(y_train, 10, dtype=int)
-
-        return x_train, y_train, x_test, y_test
-
+        print(f"batch_size: {self.batch_size}")
+        print(f"learning_rate: {self.learning_rate}")
+        print(f"optimizer: {self.optimizer}")
 
     def train(self):
         x_train, y_train, x_test, y_test = self.get_model_data()
         print(x_train.shape, y_train.shape, x_test.shape, y_test.shape)
-        opt = SGD(learning_rate=0.1)
-        self.model.compile(loss='categorical_crossentropy', optimizer=opt)
+
+        y_train_cat = utils.to_categorical(y_train, 10)
+        y_test_cat = utils.to_categorical(y_test, 10)
+        opt = SGD(learning_rate=self.learning_rate)
+        self.model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
         history = self.model.fit(x_train, y_train,
             batch_size=self.batch_size,
-            epochs=20,
+            epochs=100,
             verbose=1,
-            validation_data=(x_test, y_test)
+            validation_data=(x_test, y_test),
         )
         with open('training_history', 'wb') as file_pi:
             pickle.dump(history.history, file_pi)
         self.model.save('trained_model.h5')
 
-    # def predit(self):
-    #     with open('training_history', 'rb') as fo:
-    #         data = pickle.load(fo)
-    #         print(data)
+    def predit(self):
+        with open('training_history', 'rb') as fo:
+            data = pickle.load(fo)
+            print(data)
+            print(len(data["loss"]))
+
+
 
 
 quiz = Quiz5()
 # quiz.VGG16()
 # quiz.train()
-quiz.predit()
-
+# quiz.predit()
 
