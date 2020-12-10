bcf06fef1564ea459bbcb75e521c3f392f2d0b68
iknowright <changchaishi@gmail.com>
2020-12-11 01:17:51 +0800

    feat(quiz3): done quiz3 and format code with black

--- hw2.py
+++ hw2.py
@@ -2,27 +2,41 @@ import sys
 from functools import partial
 
 # GUI use case
-from PyQt5.QtWidgets import QDesktopWidget, QVBoxLayout, QHBoxLayout, QFormLayout, QDialog
-from PyQt5.QtWidgets import QPushButton, QGroupBox, QLineEdit, QApplication, QMessageBox, QLabel, QComboBox
+from PyQt5.QtWidgets import (
+    QDesktopWidget,
+    QVBoxLayout,
+    QHBoxLayout,
+    QFormLayout,
+    QDialog,
+)
+from PyQt5.QtWidgets import (
+    QPushButton,
+    QGroupBox,
+    QLineEdit,
+    QApplication,
+    QMessageBox,
+    QLabel,
+    QComboBox,
+)
 from PyQt5.QtGui import QDoubleValidator, QIntValidator, QFont
 from PyQt5.QtCore import Qt, pyqtSlot
 
 from quiz import Quiz1, Quiz2, Quiz3, Quiz4
 
-class App(QDialog):
 
+class App(QDialog):
     def __init__(self):
         super().__init__()
         self.initUI()
 
-
     def center(self):
         frameGm = self.frameGeometry()
-        screen = QApplication.desktop().screenNumber(QApplication.desktop().cursor().pos())
+        screen = QApplication.desktop().screenNumber(
+            QApplication.desktop().cursor().pos()
+        )
         centerPoint = QApplication.desktop().screenGeometry(screen).center()
         frameGm.moveCenter(centerPoint)
         self.move(frameGm.topLeft())
-
 
     def initUI(self):
 
@@ -39,7 +53,6 @@ class App(QDialog):
         self.center()
         self.show()
 
-
     def createQuiz1(self):
         self.quiz1 = Quiz1()
 
@@ -47,14 +60,13 @@ class App(QDialog):
         quiz1_layout = QVBoxLayout()
         quiz1_layout.setAlignment(Qt.AlignTop)
 
-        btn_11 = QPushButton('1.1 Background Subtraction')
+        btn_11 = QPushButton("1.1 Background Subtraction")
         btn_11.clicked.connect(self.quiz1.background_subtraction)
         quiz1_layout.addWidget(btn_11)
 
         quiz1groupbox.setLayout(quiz1_layout)
 
         return quiz1groupbox
-
 
     def createQuiz2(self):
         self.quiz2 = Quiz2()
@@ -63,18 +75,17 @@ class App(QDialog):
         quiz2_layout = QVBoxLayout()
         quiz2_layout.setAlignment(Qt.AlignTop)
 
-        btn_21 = QPushButton('2.1 Preprocessing')
+        btn_21 = QPushButton("2.1 Preprocessing")
         btn_21.clicked.connect(self.quiz2.preprocessing)
         quiz2_layout.addWidget(btn_21)
 
-        btn_21 = QPushButton('2.2 Video Tracking')
+        btn_21 = QPushButton("2.2 Video Tracking")
         btn_21.clicked.connect(self.quiz2.optical_flow)
         quiz2_layout.addWidget(btn_21)
 
         quiz2groupbox.setLayout(quiz2_layout)
 
         return quiz2groupbox
-
 
     def createQuiz3(self):
         self.quiz3 = Quiz3()
@@ -83,8 +94,8 @@ class App(QDialog):
         quiz3_layout = QVBoxLayout()
         quiz3_layout.setAlignment(Qt.AlignTop)
 
-        btn_31 = QPushButton('3.1 Perspective Transform')
-        # btn_31.clicked.connect()
+        btn_31 = QPushButton("3.1 Perspective Transform")
+        btn_31.clicked.connect(self.quiz3.marker)
         quiz3_layout.addWidget(btn_31)
 
         quiz3groupbox.setLayout(quiz3_layout)
@@ -98,11 +109,11 @@ class App(QDialog):
         quiz4_layout = QVBoxLayout()
         quiz4_layout.setAlignment(Qt.AlignTop)
 
-        btn_41 = QPushButton('4.1 Image Reconstruction')
+        btn_41 = QPushButton("4.1 Image Reconstruction")
         # btn_41.clicked.connect()
         quiz4_layout.addWidget(btn_41)
 
-        btn_42 = QPushButton('4.2 Compute Reconstruction Error')
+        btn_42 = QPushButton("4.2 Compute Reconstruction Error")
         # btn_42.clicked.connect()
         quiz4_layout.addWidget(btn_42)
 
@@ -111,7 +122,7 @@ class App(QDialog):
         return quiz4groupbox
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = QApplication(sys.argv)
     ex = App()
     sys.exit(app.exec_())
--- quiz.py
+++ quiz.py
@@ -12,10 +12,9 @@ class Subtractor:
         self.train(train_frames)
 
     def train(self, train_frames):
-        frames = np.array([
-            cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-            for frame in train_frames
-        ])
+        frames = np.array(
+            [cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY) for frame in train_frames]
+        )
 
         self.pixels = np.moveaxis(frames, 0, -1)
         height, width, n = self.pixels.shape
@@ -39,22 +38,21 @@ class Subtractor:
         return new_frame
 
 
-class Quiz1():
+class Quiz1:
     def __init__(self):
         pass
 
     def background_subtraction(self):
-        cap = cv2.VideoCapture('src/Q1_Image/bgSub.mp4')
+        cap = cv2.VideoCapture("src/Q1_Image/bgSub.mp4")
 
         fps = cap.get(cv2.CAP_PROP_FPS)
 
         frames = []
-        while(cap.isOpened()):
+        while cap.isOpened():
             ret, frame = cap.read()
             if not ret:
                 break
             frames.append(frame)
-
 
         train = frames[:50]
         test = frames[50:]
@@ -63,23 +61,23 @@ class Quiz1():
         # render
         for frame in test:
             subframe = model.apply(frame)
-            stacked_subframe = np.stack((subframe,)*3, axis=-1)
-            demoframe = cv2.hconcat([frame,stacked_subframe])
-            cv2.imshow('Demo', demoframe)
-            if cv2.waitKey(25) & 0xFF == ord('q'):
+            stacked_subframe = np.stack((subframe,) * 3, axis=-1)
+            demoframe = cv2.hconcat([frame, stacked_subframe])
+            cv2.imshow("Demo", demoframe)
+            if cv2.waitKey(25) & 0xFF == ord("q"):
                 break
 
 
-class Quiz2():
+class Quiz2:
     def __init__(self):
         self.frame = None
         self.points = None
 
     def get_points(self):
-        cap = cv2.VideoCapture('src/Q2_Image/opticalFlow.mp4')
+        cap = cv2.VideoCapture("src/Q2_Image/opticalFlow.mp4")
 
         first = None
-        while(cap.isOpened() and first is None):
+        while cap.isOpened() and first is None:
             ret, frame = cap.read()
             first = frame
 
@@ -99,12 +97,14 @@ class Quiz2():
 
         pts = []
         for i in keypoints:
-            x,y = i.pt
+            x, y = i.pt
             x = round(x)
             y = round(y)
             if x < 500:
                 pts.append(i.pt)
-                frame = cv2.rectangle(frame, (x-5, y-5), (x+5,y+5), (0, 0, 255), 1)
+                frame = cv2.rectangle(
+                    frame, (x - 5, y - 5), (x + 5, y + 5), (0, 0, 255), 1
+                )
 
         self.points = np.asarray(pts).reshape((len(pts), 1, 2))
         self.frame = frame
@@ -118,16 +118,18 @@ class Quiz2():
         if self.frame is None:
             self.get_points()
 
-        cap = cv2.VideoCapture('src/Q2_Image/opticalFlow.mp4')
+        cap = cv2.VideoCapture("src/Q2_Image/opticalFlow.mp4")
         # skip first frame
         ret, frame = cap.read()
         # Parameters for lucas kanade optical flow
-        lk_params = dict( winSize  = (21,21),
-                        maxLevel = 2,
-                        criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+        lk_params = dict(
+            winSize=(21, 21),
+            maxLevel=2,
+            criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03),
+        )
 
         # Create some random colors
-        color = np.random.randint(0,255,3)
+        color = np.random.randint(0, 255, 3)
 
         # Take first frame and find corners in it
         old_frame = self.frame
@@ -136,44 +138,107 @@ class Quiz2():
         p0 = self.points.astype(np.float32)
         # Create a mask image for drawing purposes
         mask = np.zeros_like(old_frame)
-        while(1):
+        while 1:
             try:
-                ret,frame = cap.read()
+                ret, frame = cap.read()
                 frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             except:
                 break
 
             # calculate optical flow
-            p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
+            p1, st, err = cv2.calcOpticalFlowPyrLK(
+                old_gray, frame_gray, p0, None, **lk_params
+            )
             # Select good points
-            good_new = p1[st==1]
-            good_old = p0[st==1]
+            good_new = p1[st == 1]
+            good_old = p0[st == 1]
             # draw the tracks
-            for i,(new,old) in enumerate(zip(good_new, good_old)):
-                a,b = new.ravel()
-                c,d = old.ravel()
-                mask = cv2.line(mask, (a,b),(c,d), color.tolist(), 1)
+            for i, (new, old) in enumerate(zip(good_new, good_old)):
+                a, b = new.ravel()
+                c, d = old.ravel()
+                mask = cv2.line(mask, (a, b), (c, d), color.tolist(), 1)
                 a = int(round(a))
                 b = int(round(b))
-                frame = cv2.rectangle(frame, (a-5, b-5), (a+5,b+5), (0, 0, 255), 1)
+                frame = cv2.rectangle(
+                    frame, (a - 5, b - 5), (a + 5, b + 5), (0, 0, 255), 1
+                )
 
-            img = cv2.add(frame,mask)
-            cv2.imshow('frame',img)
-            k = cv2.waitKey(30) & 0xff
+            img = cv2.add(frame, mask)
+            cv2.imshow("frame", img)
+            k = cv2.waitKey(30) & 0xFF
             if k == 27:
                 break
             # Now update the previous frame and previous points
             old_gray = frame_gray.copy()
-            p0 = good_new.reshape(-1,1,2)
+            p0 = good_new.reshape(-1, 1, 2)
 
         cap.release()
 
 
-class Quiz3():
+class Quiz3:
     def __init__(self):
         pass
 
+    def marker(self):
+        cap = cv2.VideoCapture("src/Q3_Image/test4perspective.mp4")
+        source = cv2.imread("src/Q3_Image/rl.jpg")
+        height, width, _ = source.shape
 
-class Quiz4():
+        d = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)
+        params = cv2.aruco.DetectorParameters_create()
+        id_sequence = [25, 33, 30, 23]
+        edges = [1, 2, 0, 0]
+
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                break
+            try:
+                (
+                    marker_corners,
+                    marker_ids,
+                    rejected_candidates,
+                ) = cv2.aruco.detectMarkers(frame, d, parameters=params)
+
+                edge_points = []
+                for i, marker_id in enumerate(id_sequence):
+                    index = np.squeeze(np.where(marker_ids == marker_id))[0]
+                    edge_points.append(np.squeeze(marker_corners[index])[edges[i]])
+
+                distance = np.linalg.norm(edge_points[0] - edge_points[1])
+                offset = round(distance * 0.02)
+
+                operators = [[-1, -1], [1, -1], [1, 1], [-1, 1]]
+                for i, operator in enumerate(operators):
+                    edge_points[i][0] += operator[0] * offset
+                    edge_points[i][1] += operator[1] * offset
+
+                source_points = np.array(
+                    [[0, 0], [width, 0], [width, height], [0, height]]
+                )
+
+                M, mask = cv2.findHomography(
+                    source_points, np.array(edge_points), cv2.RANSAC
+                )
+
+                w, h, _ = frame.shape
+                mask_img = cv2.warpPerspective(source, M, (h, w))
+                ret, mask = cv2.threshold(
+                    cv2.cvtColor(mask_img, cv2.COLOR_BGR2GRAY),
+                    0,
+                    255,
+                    cv2.THRESH_BINARY_INV,
+                )
+                new_frame = cv2.bitwise_and(frame, frame, mask=mask)
+                new_frame = cv2.bitwise_or(new_frame, mask_img)
+
+                cv2.imshow("new_frame", new_frame)
+            except:
+                pass
+            if cv2.waitKey(25) & 0xFF == ord("q"):
+                break
+
+
+class Quiz4:
     def __init__(self):
         pass
